76b2d0edf63a6db52979fca620b32cb45a253c86
degarces <degarces04@hotmail.com>
2025-10-02 16:04:01 +0100

    Small text changes

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
       {/* Hero */}
-      <main className="row-start-2 px-8 sm:px-20 py-16 flex flex-col gap-8 items-center sm:items-start">
+      <main className="row-start-2 px-8 sm:px-20 py-10 flex flex-col gap-8 items-center sm:items-start">
         
         <h1 className=" text-4xl sm:text-5xl font-extrabold tracking-tight">
           Welcome to <span className="text-green-400">ThorneCheck</span>
         </h1>
-        <p className=" text-lg ">
+        <p className="font-sans text-lg ">
           ThorneCheck is the best client for your desired purposes ...
         </p>
 
@@ -32,7 +32,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="row-start-3 py-6 text-center text-sm text-gray-500">
-        <span>© {new Date().getFullYear()} Your Name</span>
+        <span>© {new Date().getFullYear()} ThorneCheck</span>
       </footer>
     </div>
   );
